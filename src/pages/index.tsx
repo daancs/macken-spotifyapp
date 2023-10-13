9e517404a7f53409089ddeeacedbcc0b778661dc
Dadi Andrason <dadi.andrason@gmail.com>
2023-10-13 14:17:18 +0200

    added types for providers in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
-import type { GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from "next";
+import type { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from "next";
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "~/server/auth";
 import Head from "next/head";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import type { Provider } from "next-auth/providers";
 
-import { type Provider } from "next-auth/providers";
 
-const Home: NextPage = ({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+
+
+
+
+
+type HomeProps = {
+  providers: Record<string, Provider>;
+};
+
+const Home: NextPage<HomeProps> = ({ providers }) => {
   const { data: sessionData } = useSession();
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
   // const {data: accessToken} = api.spotify.getAccessToken.useQuery();
@@ -16,6 +25,7 @@ const Home: NextPage = ({ providers }: InferGetServerSidePropsType<typeof getSer
   // if (accessToken) {
   //   // const {data: currentSong} = api.spotify.getCurrentSong.useQuery({access_token: accessToken.access_token});
   // }
+
   return (
     <>
       <Head>
@@ -26,17 +36,17 @@ const Home: NextPage = ({ providers }: InferGetServerSidePropsType<typeof getSer
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0a4a21] to-[#102615]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Log in to <span className="text-[#1ed760]">macken-spotify</span> App
+            Log in to <span className="from-[#1ed760] text-transparent to-[#4cf3e5] bg-gradient-to-tl bg-clip-text">macken-spotify</span> App
           </h1>
-         
-          {Object.values(providers).map((provider) => (
+
+          {providers ? Object.values(providers).map((provider) => (
             <div key={provider.name}>
               {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-              <button className="flex w-[10vw} h-10 bg-[#525252] p-4 text-center items-center" onClick={() => signIn(provider.id, {callbackUrl: "/playback", redirect: false })}>
+              <button className="flex text-white font-bold text-2xl w-[20vw] h-16 transition-all justify-center hover:bg-[rgba(20,20,20,0.9)] bg-[rgba(20,20,20,0.5)] p-4 items-center rounded-lg" onClick={() => signIn(provider.id, { callbackUrl: "/playback", redirect: false })}>
                 Sign in with {provider.name}
               </button>
             </div>
-          ))}
+          )) : ""}
         </div>
       </main>
     </>
@@ -47,7 +57,7 @@ export default Home;
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await getServerSession(context.req, context.res, authOptions);
-  
+
   // If the user is already logged in, redirect.
   // Note: Make sure not to redirect to the same page
   // To avoid an infinite loop!
@@ -56,8 +66,10 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   }
 
   const providers = await getProviders();
-  
+
   return {
-    props: { providers: providers ?? [] },
+    props: {
+      providers: providers
+    },
   }
 }
